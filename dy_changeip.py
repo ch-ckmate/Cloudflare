7088521f36f052a6a3587fafa542c26f5dee77b7
0x00 <checkmaster0@gmail.com>
2023-05-25 08:15:36 +0200

    Add files via upload

---
+++ dy_changeip.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+myip = requests.get("http://ifconfig.me").content.decode("UTF-8")
+dnsname='sub.example.com'
+dw = requests.get("https://api.cloudflare.com/client/v4/zones/{zoneid}/dns_records",headers={"Authorization": "Bearer token", "Content-Type": "application/json"}).content.decode("UTF-8")
+for dns in json.loads(dw)['result']:
+    if dnsname in dns['name']:
+        if dns['content']!=myip:
+            requests.put("https://api.cloudflare.com/client/v4/zones/{zoneid}/dns_records",headers={"Authorization": "Bearer token", "Content-Type": "application/json"}
+                         ,json={"type":"A","name":dnsname,"content":myip})
+
